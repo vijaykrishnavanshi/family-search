85b4202113793c8f325058c700b4a11645fd3362
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2019-04-14 05:57:05 +0530

    Setup basic express server

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+// Express App
+const app = express();
+
+// Use default logger for now
+app.use(logger("combined"));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// This is to check if the service is online or not
+app.use("/ping", function(req, res) {
+  res.json({ reply: "pong" });
+  res.end();
+});
+
+// Export the express app instance
+module.exports = app;
