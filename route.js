8cd3b9ab95fa16adedb86329617f8c7c2839352e
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2019-04-28 16:34:18 +0530

    Complete loop from searching a user using elastic search and getting the node from neo4j

--- route.js
+++ route.js
@@ -7,11 +7,11 @@ const neo4j = require("./db/neo4j");
 router.route("/search-elasticsearch").get((req, res) => {
   console.log(req.query);
   elasticsearch
-  .search({
-    index: "nameindex",
-    q: (req.query && req.query.term) || "geller"
-  })
-  .then(response => {
+    .search({
+      index: "nameindex",
+      q: (req.query && req.query.term) || "geller"
+    })
+    .then(response => {
       console.log(response.hits.hits);
       return res.json({ hello: response.hits.hits });
     })
@@ -47,7 +47,7 @@ router.route("/search-neo4j").get((req, res) => {
 });
 
 router.route("/search").get((req, res) => {
-  console.log(req.query);
+  console.log("req.query: ", req.query);
   elasticsearch
     .search({
       index: "nameindex",
@@ -55,8 +55,29 @@ router.route("/search").get((req, res) => {
     })
     .then(response => {
       console.log("response: ", response);
+      console.log("response.hits.hits: ", response.hits.hits);
       console.log(response.hits.hits);
-      return res.json({ hello: response.hits.hits });
+      const { hits } = response.hits;
+      const firstHit = hits.shift();
+      const { id } = firstHit._source;
+      const session = neo4j.session();
+      const cypherQuery = `MATCH (cast:CAST {id: {id}}) RETURN cast`;
+      const params = { id };
+      console.log(cypherQuery);
+      return session.run(cypherQuery, params);
+    })
+    .then(results => {
+      console.log(results);
+      results.records.map(record => console.log(record.get("cast")));
+      const record = results.records.shift();
+      if (!record) {
+        console.log("No user found.");
+        res.json({ hello: null });
+      } else {
+        const castNode = record.get("cast");
+        console.log(JSON.stringify(castNode, null, 4));
+        res.json({ hello: castNode.properties });
+      }
     })
     .catch(error => {
       console.trace(error.message);
